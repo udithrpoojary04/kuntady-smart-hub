0c0dcafd848214f7bf5aafe4ad5f2843ad21c8a9
udithrpoojary04 <udithrpoojary@gmail.com>
2026-02-22 19:18:27 +0530

    fix: restore STATIC_URL for django collectstatic during render deployment

--- backend/transport_project/settings.py
+++ backend/transport_project/settings.py
@@ -129,6 +129,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Cloudinary Integration
